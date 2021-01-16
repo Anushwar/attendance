734edb86c817ea340f4a86f4efdf80be831b3911
Jeevan Surendran <jvns67@gmail.com>
2021-01-17 01:42:59 +0530

    Fix login issue with states

--- attendance-portal/src/Home/index.js
+++ attendance-portal/src/Home/index.js
@@ -1,7 +1,13 @@
 /** @jsxImportSource @emotion/core */
 import { Button } from '@chakra-ui/core';
 import styled from '@emotion/styled';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  updateAdminUser,
+  updateStudentUser,
+  updateTeacherUser,
+} from '../redux/actions';
 
 const Container = styled.div({
   display: 'flex',
@@ -15,15 +21,34 @@ const HomeButton = styled(Button)({
 });
 
 const Admin = () => {
+  const dispatch = useDispatch();
+  const adminUser = useSelector(({ adminData }) => adminData.user);
+  const teacherUser = useSelector(({ teacherData }) => teacherData.user);
+  const studentUser = useSelector(({ studentData }) => studentData.user);
   return (
     <Container>
-      <Link to="/admin">
+      <Link
+        to="/admin"
+        onClick={() => {
+          if (adminUser) dispatch(updateAdminUser(adminUser));
+        }}
+      >
         <HomeButton>Admin Dashboard</HomeButton>
       </Link>
-      <Link to="/student">
+      <Link
+        to="/student"
+        onClick={() => {
+          if (studentUser) dispatch(updateStudentUser(studentUser));
+        }}
+      >
         <HomeButton>Student Dashboard</HomeButton>
       </Link>
-      <Link to="/teacher">
+      <Link
+        to="/teacher"
+        onClick={() => {
+          if (teacherUser) dispatch(updateTeacherUser(teacherUser));
+        }}
+      >
         <HomeButton>Teacher Dashboard</HomeButton>
       </Link>
     </Container>
